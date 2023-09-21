198ba0a6b4626dfa05d472ad89b83a6c8b5b1852
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2023-09-21 13:19:59 -0400

    add openaq info

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://api.openaq.org/v2/locations?parameters=pm25"
+res = requests.get(url)
+data = res.json()
 
 def getdata(url):
   r= requests.get(url)
