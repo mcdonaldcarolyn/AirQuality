3e9a0dd47fa133036dea4f40e3dc25ea5a4d4df6
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2023-09-27 08:52:28 -0400

    add location_id

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = "https://api.openaq.org/v2/locations?parameters=pm25"
+location_id = 355579
+url = "https://api.openaq.org/v2/locations/{location_id}"
 #url = https://api.openaq.org/v2/measurements?location_id=355579&parameter=um025&parameter=pm10&parameter=um100&parameter=um050&parameter=um003&parameter=pressure&parameter=temperature&parameter=humidity&parameter=um005&parameter=um010&parameter=pm1&parameter=pm25&date_from=2023-09-25T14:05:17-04:00&date_to=2023-09-26T14:05:17-04:00&limit=1000
 res = requests.get(url)
 data = res.json()
+header = 
 
 def getdata(url):
   r= requests.get(url)
